a37dde1b78972ec69a93a7deeda6b61c74d29f61
pravin-python <pravin.prajapati0126@gmail.com>
2026-03-02 18:39:42 +0530

    feat: Add sitemap and robots.txt for improved SEO and update base paths and canonical URLs.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Web-OS-Portfolio/",
+  base: "./",
   plugins: [react(), tailwindcss()],
   test: {
     globals: true,
