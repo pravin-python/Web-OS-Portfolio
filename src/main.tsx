b3ffc92dee9478009614c5a8617fb8bacd1e032a
pravin-python <pravin.prajapati0126@gmail.com>
2026-03-03 10:22:31 +0530

    feat: Initialize React application entry point in `main.tsx` and update `index.html` base paths for deployment.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 
@@ -9,8 +9,8 @@ window.addEventListener("contextmenu", (e) => e.preventDefault());
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
